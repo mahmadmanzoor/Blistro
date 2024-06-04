52d18a93c004379c26df689dc879771ba2a338d0
mahmadmanzoor <62117177+mahmadmanzoor@users.noreply.github.com>
2024-06-04 16:36:38 +0500

    Link Preview and Deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
         <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
           <p className='text-sm font-semibold text-gray-700'>
-            Blistro is now public!
+            Quill is now public!
           </p>
         </div>
         <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
@@ -19,7 +19,7 @@ export default function Home() {
           in seconds.
         </h1>
         <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
-          Blistro allows you to have conversations with any
+          Quill allows you to have conversations with any
           PDF document. Simply upload your file and start
           asking questions right away.
         </p>
@@ -91,7 +91,7 @@ export default function Home() {
             </h2>
             <p className='mt-4 text-lg text-gray-600'>
               Chatting to your PDF files has never been
-              easier than with Blistro.
+              easier than with Quill.
             </p>
           </div>
         </div>
@@ -141,7 +141,7 @@ export default function Home() {
                 Start asking questions
               </span>
               <span className='mt-2 text-zinc-700'>
-                It&apos;s that simple. Try out Blistro today -
+                It&apos;s that simple. Try out Quill today -
                 it really takes less than a minute.
               </span>
             </div>
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,33 +3,33 @@
 import { trpc } from '@/app/_trpc/client'
 import { absoluteUrl } from '@/lib/utils'
 import {
-    QueryClient,
-    QueryClientProvider,
+  QueryClient,
+  QueryClientProvider,
 } from '@tanstack/react-query'
 import { httpBatchLink } from '@trpc/client'
 import { PropsWithChildren, useState } from 'react'
 
 const Providers = ({ children }: PropsWithChildren) => {
-    const [queryClient] = useState(() => new QueryClient())
-    const [trpcClient] = useState(() =>
-        trpc.createClient({
-            links: [
-                httpBatchLink({
-                    url: "https://localhost:3000/api/trpc",
-                }),
-            ],
-        })
-    )
+  const [queryClient] = useState(() => new QueryClient())
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: absoluteUrl("/api/trpc"),
+        }),
+      ],
+    })
+  )
 
-    return (
-        <trpc.Provider
-            client={trpcClient}
-            queryClient={queryClient}>
-            <QueryClientProvider client={queryClient}>
-                {children}
-            </QueryClientProvider>
-        </trpc.Provider>
-    )
+  return (
+    <trpc.Provider
+      client={trpcClient}
+      queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </trpc.Provider>
+  )
 }
 
 export default Providers
